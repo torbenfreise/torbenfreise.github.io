97d280da898feb96f2eee1626fc31e33f7c40777
drumglow <t.a.freise@student.vu.nl>
2026-02-05 19:04:54 +0100

    Remove pre page load url access

--- src/routes/projects/totp/+page.ts
+++
@@ -1,9 +0,0 @@
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = ({url }) => {
-	return {
-		post: {
-			secret: url.searchParams.get('secret') ?? 'ORXXEYTFNYXGM4TFNFZWKQDHNVQWS3BOMNXW2SCFJZHEORKDJBAUYTCFJZDUKMBQGQ======'
-		}
-	};
-};
