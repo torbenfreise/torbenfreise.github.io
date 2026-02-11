1be6f2f0891929e01365b47a07cb6e9761e37785
Torben Freise <t.a.freise@student.vu.nl>
2026-02-11 19:44:22 +0100

    Add meta tags to all pages

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -5,6 +5,11 @@
 	const { content: Content, meta } = data;
 </script>
 
+<svelte:head>
+	<title>{meta.title}</title>
+	<meta name="description" content={meta.excerpt} />
+</svelte:head>
+
 <article class="prose max-w-none dark:prose-invert">
 	<header class="mb-8">
 		<h1>{meta.title}</h1>
